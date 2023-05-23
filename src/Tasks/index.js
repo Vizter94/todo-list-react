86153891822b4deea98988e3d73104aac62a4707
Krzysiek Zawiasiński <evil753@gmail.com>
2023-05-23 06:42:09 +0200

    Desturization for props

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
-const Tasks = (props) => (
+const Tasks = ({ tasks,hideDoneTasks }) => (
     <ul className="list">
-        {props.tasks.map(tasks => (
-            <li key={tasks.id} className={`list__items ${tasks.done && props.hideDoneTasks ? "list__items--hidden" : "" } ${tasks.done && "list__items--done"}`}>
+        {tasks.map(tasks => (
+            <li key={tasks.id} className={`list__items ${tasks.done && hideDoneTasks ? "list__items--hidden" : "" } ${tasks.done ? "list__items--done" : ""}`}>
                 <button className={`list__button--notdone ${tasks.done && "list__button--done"}`}></button>
                 <p className="list__text"> {tasks.content}</p>
                 <button className="list__button--delete">
