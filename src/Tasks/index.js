671d29986b202b33093ad16927ae794906640da6
Krzysiek Zawiasiński <evil753@gmail.com>
2023-09-23 06:35:51 +0200

    change named

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -6,7 +6,8 @@ const Tasks = ({ tasks, hideDone, removeTask }) => (
             <li key={tasks.id} className={`list__items ${tasks.done && hideDone ? "list__items--hidden" : "" } ${tasks.done ? "list__items--done" : ""}`}>
                 <button className={`list__button--notdone ${tasks.done && "list__button--done"}`}></button>
                 <p className="list__text"> {tasks.content}</p>
-                <button className="list__button--delete" onClick={() => removeTask(task.id)} >
+                <button className="list__button--delete" 
+                onClick={() => removeTask(tasks.id)} >
                 </button>
             </li>
         ))}
