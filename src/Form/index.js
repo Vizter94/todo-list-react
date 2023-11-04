0b97cc8e20802e6f9fa413731684664556df4a58
Krzysiek Zawiasiński <evil753@gmail.com>
2023-11-04 11:19:32 +0100

    added margin after input and change class name in button

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,22 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({addNewTask}) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        addNewTask(newTaskContent.trim());
-        setNewTaskContent("");
-    }
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
+  };
 
-    return (
-        <form className="form" onSubmit={onFormSubmit} >
-            <input value={newTaskContent} className="form__input" placeholder="Co jest do zrobienia?" onChange={({target}) => setNewTaskContent(target.value) } />
-            <button className="form__button">Dodaj zadanie</button>
-        </form>
-    );
-}
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        className="form__input"
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="button">Dodaj zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
-
