642e43cae7242fe04a34d7d8205e78e4f825e882
Krzysiek Zawiasiński <evil753@gmail.com>
2023-11-04 11:22:29 +0100

    Formating code

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,19 +1,28 @@
 import "./style.css";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="list">
-        {tasks.map(tasks => (
-            <li key={tasks.id} className={`list__items ${tasks.done && hideDone ? "list__items--hidden" : "" } ${tasks.done ? "list__items--done" : ""}`}>
-                <button 
-                onClick={() => toggleTaskDone(tasks.id)}
-                className={`list__button--notdone ${tasks.done && "list__button--done"}`}></button>
-                <p className="list__text"> {tasks.content}</p>
-                <button className="list__button--delete" 
-                onClick={() => removeTask(tasks.id)} >
-                </button>
-            </li>
-        ))}
-    </ul >
+  <ul className="list">
+    {tasks.map((tasks) => (
+      <li
+        key={tasks.id}
+        className={`list__items ${
+          tasks.done && hideDone ? "list__items--hidden" : ""
+        } ${tasks.done ? "list__items--done" : ""}`}
+      >
+        <button
+          onClick={() => toggleTaskDone(tasks.id)}
+          className={`list__button--notdone ${
+            tasks.done && "list__button--done"
+          }`}
+        ></button>
+        <p className="list__text"> {tasks.content}</p>
+        <button
+          className="list__button--delete"
+          onClick={() => removeTask(tasks.id)}
+        ></button>
+      </li>
+    ))}
+  </ul>
 );
 
 export default Tasks;
