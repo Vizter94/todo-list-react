cb85cbf7b41f4abf2ca778499a8c4e16182e52b6
Krzysiek Zawiasiński <evil753@gmail.com>
2024-02-08 12:40:40 +0100

    Added tagRef in input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormStyled, Button, Input } from "./styled";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
 
+  const tagRef = useRef(null);
+
   const onFormSubmit = (event) => {
     event.preventDefault();
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    tagRef.current.focus();
   };
 
   return (
     <FormStyled onSubmit={onFormSubmit}>
       <Input
+        ref={tagRef}
         value={newTaskContent}
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
